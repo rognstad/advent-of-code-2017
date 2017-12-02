4fb49fad09f54d3a813b041d79f739b2b6b83ece
Matt Rognstad <rognstad@gmail.com>
2017-12-02 09:53:15 -0600

    Add day 2

---
+++ day2.js
@@ -0,0 +1,59 @@
+function getRows(str) {
+    return str.split(/\n/);
+}
+
+function cleanRow(row) {
+    return row.split(/\s+/).map(Number);
+}
+
+// wrap multi-line input in ``
+function aoc2017d2p1(input) {
+
+    function getRowChecksum(row) {
+        var max, min;
+        row.forEach(function (value) {
+            if (!max || value > max) {
+                max = value;
+            }
+            if (!min || value < min) {
+                min = value;
+            }
+        });
+        return max - min;
+    }
+
+    return getRows(input)
+        .map(cleanRow)
+        .reduce(function (sum, row) {
+            return sum + getRowChecksum(row);
+        }, 0);
+}
+
+// wrap multi-line input in ``
+function aoc2017d2p2(input) {
+
+    function sortHighestToLowest(row) {
+        return row.sort(function (a, b) {
+            return b - a;
+        });
+    }
+
+    function getRowChecksum(input) {
+        var row = [...input];
+        var numerator = row.shift();
+
+        for (let i = 0; i < row.length; i++) {
+            if (numerator % row[i] === 0) {
+                return numerator / row[i];
+            }
+        }
+
+        return getRowChecksum(row);
+    }
+
+    return getRows(input)
+        .map(cleanRow)
+        .reduce(function (sum, row) {
+            return sum + getRowChecksum(sortHighestToLowest(row));
+        }, 0);
+}
